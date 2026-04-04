d0374d26e01e2207e144f4300dc64bf3c39ac061
Dan <Dan@DAN-LAPTOP>
2026-04-03 22:24:32 -0500

    Finished Weather Application in week10

--- week10/Weather Application Using API/script.js
+++ week10/Weather Application Using API/script.js
@@ -1,66 +1,65 @@
-// Add event listener to button
 document.getElementById("weatherBtn").addEventListener("click", getWeather);
 
-// Function to convert weather code to icon
 function getWeatherIcon(code) {
+  if (code === 0) return "☀️";
+  if ([1,2,3].includes(code)) return "☁️";
+  if ([51,53,55,61,63,65].includes(code)) return "🌧️";
+  if ([95].includes(code)) return "⛈️";
+  if ([71,73,75].includes(code)) return "❄️";
+  if ([45,48].includes(code)) return "🌫️";
 
-  // TODO: Add conditions for weather codes
-  // Example:
-  // if (code === 0) return "☀️";
-
-  return "🌍"; // default icon
+  return "🌍"; 
 }
 
 async function getWeather() {
-
   const output = document.getElementById("output");
-
-  // Step 1: Get city input
   let city = document.getElementById("city").value.trim();
 
-  // TODO: Handle empty input
-  // If city is empty → show error message
+  if (!city) {
+    output.innerHTML = "<p style='color:red;'>Input is empty</p>";
+    return;
+  }
 
   try {
-
-    // Step 2: Call Geocoding API
-    let geoURL = `https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(city)}&count=1`;
+    let geoURL =`https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(city)}&count=10&language=en&format=json`;
 
     let geoResponse = await fetch(geoURL);
 
-    // TODO: Check if response is OK
+    if (!geoResponse.ok) {
+      throw new Error('Unable to fetch Geocoding API')
+    }
 
     let geoData = await geoResponse.json();
 
-    // TODO: Check if city exists in response
+    if(!geoData.results || geoData.results.length === 0) {
+      output.innerHTML = "<p style='color:red;'>There are no results found</p>"
+    }
 
-    // TODO: Extract latitude and longitude
-    let latitude = null;
-    let longitude = null;
+    let latitude = geoData.results[0].latitude;
+    let longitude = geoData.results[0].longitude;
     let displayCity = city;
-
-    // Step 3: Call Weather API
     let weatherURL =
       `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}` +
       `&current=temperature_2m,relative_humidity_2m,wind_speed_10m,weather_code` +
       `&temperature_unit=fahrenheit&wind_speed_unit=mph`;
-
     let weatherResponse = await fetch(weatherURL);
 
-    // TODO: Check if response is OK
+    if(!weatherResponse.ok) {
+      throw new Error("Unable to fetch Weather API");
+    }
 
     let weatherData = await weatherResponse.json();
 
-    // TODO: Extract required data:
-    let tempF = null;
-    let humidity = null;
-    let windMph = null;
-    let weatherCode = null;
+    if (!weatherData.current) {
+        throw new Error ("Unable to fetch weather data");
+      }
 
-    // TODO: Get icon using function
+    let tempF = weatherData.current.temperature_2m;
+    let humidity = weatherData.current.relative_humidity_2m;
+    let windMph = weatherData.current.wind_speed_10m;
+    let weatherCode = weatherData.current.weather_code;
     let icon = getWeatherIcon(weatherCode);
 
-    // Step 4: Display output
     output.innerHTML = `
       <h3>📍 ${displayCity}</h3>
       <p>🌡 Temperature: ${tempF} °F</p>
@@ -70,8 +69,6 @@ async function getWeather() {
     `;
 
   } catch (error) {
-
-    // TODO: Show error message
-    output.innerHTML = "<p style='color:red;'>Error fetching data</p>";
+    output.innerHTML = "<p style='color:red;'>Unable to identify city input</p>";
   }
 }
